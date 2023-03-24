8573b8c37ed6c589e70ec9c29c9e0e973be8a33f
bouzrbay <atighmarcelline@gmail.com>
2023-03-24 07:48:26 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,23 +8,24 @@
 
 void print_triangle(int size)
 {
-  int x, y, z;
-  if (size <= 0)
+int x, y, z;
+if (size <= 0)
 {
-  _putchar('\n');
+_putchar('\n');
 }
-  else
+else
 {
-  for (x = 0; x < size; x ++)
+for (x = 0; x < size; x ++)
 {
-  for (y = size - x; y > 1; y--)
-    _putchar(32);
+for (y = size - x; y > 1; y--)
+{
+_putchar(32);
 }
-  for (z = 0; z <= x; z++)
+for (z = 0; z <= x; z++)
 {
-  _putchar(35);
+_putchar(35);
 }
-  _putchar('\n');
+_putchar('\n');
 }
 }
 }
