45e567a372ac8c68312c181cc5acf6f1cabc0832
bouzrbay <atighmarcelline@gmail.com>
2023-03-27 09:54:34 -0700

    101-keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -15,7 +15,7 @@ int index = 0, sum = 0, diff_half1, diff_half2;
 srand(time(0));
 while (sum < 2772)
 {
-password[index] = 33 + rand() + 94;
+password[index] = 33 + rand() % 94;
 sum += password[index];
 }
 password[index] = '\0';
