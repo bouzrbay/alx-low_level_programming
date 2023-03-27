34d53cb05e342c5cb2be26e037e0b55a3775c9af
bouzrbay <atighmarcelline@gmail.com>
2023-03-27 09:58:20 -0700

    101-keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -31,11 +31,12 @@ for (index = 0; password[index]; index++)
 if (password[index] >= (33 + diff_half1))
 {
 password[index] -= diff_half1;
+break;
 }
 }
 for (index = 0; password[index]; index++)
 {
-if (password[index] >= (33 - diff_half2))
+if (password[index] >= (33 + diff_half2))
 {
 password[index] -= diff_half2;
 break;
