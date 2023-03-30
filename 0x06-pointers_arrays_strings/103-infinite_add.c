68ab3a575985ccc5379bb9537fc1a06f6ab67d69
bouzrbay <atighmarcelline@gmail.com>
2023-03-30 07:04:11 -0700

    103-add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -29,14 +29,14 @@ void rev_string(char *n)
 }
 
 /**
- *infinite_add - add 2 numbers together
+ * infinite_add - add 2 numbers together
  *
  *@n1: text representation of 1st nuber to add
  *@n2: text representation of 2nd number to add
  *@r: pointer to buffer
  *@size_r: buffer size
  *
- *Return: pointer to clling function
+ *Return: pointer to calling function
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
@@ -60,10 +60,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
       else
 	val1 = *(n1 + i) - '0';
       if (j < 0)
-	val2= 0;
+	val2 = 0;
       else
 	val2 = *(n2 + j) - '0';
-      temp_tot = val2 + overflow;
+      temp_tot = val1 + val2 + overflow;
       if (temp_tot >= 10)
 	overflow = 1;
       else
