80abab7b492727d260229bdf49ceaaeec36645f3
bouzrbay <atighmarcelline@gmail.com>
2023-04-05 09:23:25 -0700

    5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,47 +1,40 @@
 #include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i);
 
 /**
- *_sqrt_recursion -  function that returns the natural
- *square root of a number.
+ * _sqrt_recursion - function that returns the natural square root of a number
  *
- *@n: input
+ *@n: number to calculate the natural square root
  *
- *Return: If n does not have a natural square root,
- *the function should return -1
+ *Return: the natural square root
  */
-
 
 int _sqrt_recursion(int n)
 {
-int sqrt_n = _sqrt_recursion(n / 2);
-if (n < 0)
-{
-return (-1);
-/*negative numbers don't have a natural square root*/
+return (_sqrt(n, 1));
 }
-if (n == 0 || n == 1)
-{
-return (n);
-/*base case, the square root of 0 and 1 is itself*/
-}
-/*recursively calculate the square root of n/2*/
 
-/*if the square root of n/2 is -1, then n doesn't have a natural square root*/
-if (sqrt_n == -1)
+/**
+ *_sqrt - calculates natural square root
+ *
+ *@n:number to calculate the square root
+ *@i: iterate number
+ *
+ *Return: the naturale square root
+ */
+
+int _sqrt(int n, int i)
 {
+
+int sqrt = i * i;
+
+if (sqrt > n)
 return (-1);
-}
-/*check if sqrt_n is actually the square root of n*/
-if (sqrt_n * sqrt_n == n)
-{
-return (sqrt_n);
-}
-else if ((sqrt_n + 1) * (sqrt_n + 1) == n)
-{
-return (sqrt_n + 1);
-}
-else
-{
-return (-1);
-}
+
+if (sqrt == n)
+return (i);
+
+return (_sqrt(n, i + 1));
 }
