6658085e1defa0e031ba64cef4af40044055afb0
bouzrbay <atighmarcelline@gmail.com>
2023-04-20 10:06:32 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,12 +16,12 @@ int sum = 0;
 unsigned int i;
 
 if (n == 0)
-return 0;
+return (0);
 
 va_start(args, n);
 for (i = 0; i < n; i++)
 sum += va_arg(args, int);
 va_end(args);
 
-return sum;
+return (sum);
 }
