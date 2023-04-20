4fe04356bd3b04cc89de44156a24b810682c29ef
bouzrbay <atighmarcelline@gmail.com>
2023-04-19 18:05:13 -0700

    calc

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -12,21 +12,21 @@
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
 
-    while (i < 5)
-      {
-	if (strcmp(s, ops[i].op) == 0)
-	  return (ops[i].f);
+while (i < 5)
+{
+if (strcmp(s, ops[i].op) == 0)
+return (ops[i].f);
 
-	i++;
-      }
-    return (0);
+i++;
+}
+return (0);
 }
